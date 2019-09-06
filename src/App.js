6372276bf765033e4f4d7962342fca046a9865b8
idcristi <cristi.duluta@gmail.com>
2019-09-06 15:12:41 +0300

    Add repos component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     user: {},
     users: [],
+    repos: [],
     loading: false,
     alert: null,
   }
@@ -35,6 +36,12 @@ class App extends Component {
     this.setState({user: res.data, loading: false})
   }
 
+  getUserRepos = async username => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`)
+    this.setState({repos: res.data, loading: false})
+  }
+
   clearUsers = () => {
     this.setState({users: [], loading: false})
   }
@@ -46,7 +53,7 @@ class App extends Component {
   }
 
   render() {
-    const {users, user, loading} = this.state
+    const {users, user, repos, loading} = this.state
 
     return (
       <Router>
@@ -78,7 +85,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
